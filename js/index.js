546c62e94a38fb81512786ee768b4b6ec525eba8
Justin Russell <Justin@Justins-MacBook.local>
2020-02-10 14:54:13 -0800

    MVP and one stretch goal done

--- js/index.js
+++ js/index.js
@@ -67,5 +67,71 @@ newNav2.style.color = "green";
 
 navSection.forEach(item => item.style.color = "green");
 
+// Logo
 
+const mainImg = document.querySelector('#cta-img')
+
+mainImg.src = siteContent.cta["img-src"];
+
+// H1 Content & Button
+
+const mainH1 = document.querySelector('h1');
+const startBtn = document.querySelector('button');
+
+mainH1.textContent = siteContent.cta["h1"];
+startBtn.textContent = siteContent.cta["button"];
+
+//Event Listener Stretch
+
+startBtn.addEventListener("click", function () {
+  startBtn.style.backgroundColor = 'grey';
+  startBtn.style.color = 'white';
+});
+
+// main content
+
+const mainContentHead = document.querySelector(".text-content:nth-child(1) h4");
+const mainContentPar = document.querySelector(".text-content:nth-child(1) p");
+const mainContentHead2 = document.querySelector(".text-content:nth-child(2) h4");
+const mainContentPar2 = document.querySelector(".text-content:nth-child(2) p");
+
+
+mainContentHead.textContent = siteContent["main-content"]["features-h4"];
+mainContentPar.textContent = siteContent["main-content"]["features-content"];
+mainContentHead2.textContent = siteContent["main-content"]["about-h4"];
+mainContentPar2.textContent = siteContent["main-content"]["about-content"];
+
+// Middle Image
+const midImg = document.getElementById('middle-img');
+
+midImg.src = siteContent["main-content"]["middle-img-src"];
+
+// Bottom Content
+const mainContentHead3 = document.querySelector(".bottom-content .text-content:nth-child(1) h4");
+const mainContentPar3 = document.querySelector(".bottom-content .text-content:nth-child(1) p");
+const mainContentHead4 = document.querySelector(".bottom-content .text-content:nth-child(2) h4");
+const mainContentPar4 = document.querySelector(".bottom-content .text-content:nth-child(2) p");
+const mainContentHead5 = document.querySelector(".bottom-content .text-content:nth-child(3) h4");
+const mainContentPar5 = document.querySelector(".bottom-content .text-content:nth-child(3) p");
+
+mainContentHead3.textContent = siteContent["main-content"]["services-h4"];
+mainContentPar3.textContent = siteContent["main-content"]["services-content"];
+mainContentHead4.textContent = siteContent["main-content"]["product-h4"];
+mainContentPar4.textContent = siteContent["main-content"]["product-content"];
+mainContentHead5.textContent = siteContent["main-content"]["vision-h4"];
+mainContentPar5.textContent = siteContent["main-content"]["vision-content"];
+
+// Contact
+const contactHeader = document.querySelector(".contact h4");
+const contactPar = document.querySelectorAll(".contact p");
+
+contactHeader.textContent = siteContent.contact["contact-h4"];
+contactPar[0].innerHTML = siteContent.contact["address"];
+contactPar[1].innerHTML = siteContent.contact["phone"];
+contactPar[2].innerHTML = siteContent.contact["email"];
+
+//Footer
+const foot = document.querySelector("footer p");
+
+foot.textContent = siteContent.footer.copyright;
 
